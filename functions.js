0473b166dcf471edd2f80077dbd19d99bec46322
AbenezerM <abenezermulugeta41@gmail.com>
2023-02-04 23:02:29 -0600

    Optional and Required parameters

--- functions.js
+++ functions.js
@@ -1,14 +1,16 @@
 "use strict";
 // named function 
-function add(num1, num2) {
-    return num1 + num2;
+// optional parameter: num3
+function add(num1, num2, num3) {
+    return num3 ? num1 + num2 + num3 : num1 + num2;
 }
 console.log(add(5, 4));
 // arrow function 
 const subtract = (num1, num2) => num1 - num2;
 console.log(subtract(3, 1));
 // anonymous function
-const multiply = function (num1, num2) {
-    return num1 * num2;
+// required parameter: default value of num3 will be 1 unless value is set
+const multiply = function (num1, num2, num3 = 1) {
+    return num1 * num2 * num3;
 };
 console.log(multiply(3, 4));
--- functions.ts
+++ functions.ts
@@ -1,16 +1,17 @@
 // named function 
-function add(num1: number, num2: number): number {
-    return num1 + num2;
+// optional parameter: num3
+function add(num1: number, num2: number, num3?: number): number {
+    return num3 ? num1 + num2 + num3 : num1 + num2;
 }
 console.log(add(5, 4));
-
 
 // arrow function 
 const subtract = (num1: number, num2: number): number => num1 - num2;
 console.log(subtract(3, 1));
 
 // anonymous function
-const multiply = function (num1: number, num2: number): number {
-    return num1 * num2;
+// required parameter: default value of num3 will be 1 unless value is set
+const multiply = function (num1: number, num2: number, num3: number = 1): number {
+    return num1 * num2 * num3;
 }
 console.log(multiply(3, 4));
