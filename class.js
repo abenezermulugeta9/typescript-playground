46323607010981d7d29265d34aac238fddaa9a4f
AbenezerM <abenezermulugeta41@gmail.com>
2023-02-09 01:19:32 -0600

    Class with parametrized constructor

--- class.js
+++ class.js
@@ -6,3 +6,12 @@ john.id = 1;
 john.name = 'John';
 john.address = 'Highway 5';
 console.log(john);
+class Book {
+    constructor(isbn, title, author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+}
+let book1 = new Book(1231423, 'You\'re a badass!', 'Jen Sincero');
+console.log(book1);
--- class.ts
+++ class.ts
@@ -10,3 +10,18 @@ john.name = 'John';
 john.address = 'Highway 5';
 
 console.log(john);
+
+class Book {
+    isbn: number;
+    title: string;
+    author: string;
+
+    constructor(isbn: number, title: string, author: string) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+}
+
+let book1 = new Book(1231423, 'You\'re a badass!', 'Jen Sincero');
+console.log(book1); 
